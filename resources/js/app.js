72d570e0d087569fc0d113c69bac3615b2c656b9
guy-mulatu <guy.mulatu@crowdproperty.com>
2021-07-17 13:48:02 +0100

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext('2d');
+canvas.width =  window.innerWidth;
+canvas.height =  window.innerHeight;
+let particlesArray = [];
+
+class Particle {
+    
+}
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>JS Rain</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/style.css') }}" />
+    <style>
+        body {
+            font-family: 'Nunito', sans-serif;
+        }
+    </style>
+</head>
+<body>
+    <canvas id="canvas1"></canvas>
+    <h1 id="title1">Boom!</h1>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
